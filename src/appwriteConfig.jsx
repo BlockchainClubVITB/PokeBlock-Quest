0825e71e5f9f336efbedff066cf86295a96480d6
Rashi <rashi1704baranwal@gmail.com>
2024-11-29 18:29:54 +0530

    feat: updated appwrite config

--- src/appwriteConfig.jsx
+++ src/appwriteConfig.jsx
@@ -4,7 +4,7 @@ const client = new Client();
 
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("662a61da6824b9c5f45a");
+  .setProject("6749aac90038687908b7");
 
 export const account = new Account(client);
 export const database = new Databases(client);
