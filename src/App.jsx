fc5439b2b7e192695e6e701c7c312f0e20bcf2be
Rashi <rashi1704baranwal@gmail.com>
2024-12-07 01:13:47 +0530

    fix: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {
   Leaderboard,
@@ -16,46 +16,51 @@ import { AuthProvider } from "./utils/AuthContext";
 function App() {
   useEffect(() => {
     // Add event listener on component mount
-    document.addEventListener('contextmenu', (e) => {
+    document.addEventListener("contextmenu", (e) => {
       e.preventDefault();
     });
-    document.addEventListener('keydown', (e) => {
-      if(e.keyCode === 123) { // F12 key code
+    document.addEventListener("keydown", (e) => {
+      if (e.keyCode === 123) {
+        // F12 key code
         e.preventDefault();
       }
-      if(e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)) { // Ctrl+Shift+I
+      if (e.ctrlKey && e.shiftKey && e.keyCode === "I".charCodeAt(0)) {
+        // Ctrl+Shift+I
         e.preventDefault();
       }
-      if(e.ctrlKey && e.shiftKey && e.keyCode === 'C'.charCodeAt(0)) { // Ctrl+Shift+C
+      if (e.ctrlKey && e.shiftKey && e.keyCode === "C".charCodeAt(0)) {
+        // Ctrl+Shift+C
         e.preventDefault();
       }
-      if(e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)) { // Ctrl+Shift+J
+      if (e.ctrlKey && e.shiftKey && e.keyCode === "J".charCodeAt(0)) {
+        // Ctrl+Shift+J
         e.preventDefault();
       }
-      if(e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) { // Ctrl+U
+      if (e.ctrlKey && e.keyCode === "U".charCodeAt(0)) {
+        // Ctrl+U
         e.preventDefault();
       }
     });
 
     // Remove event listeners on component unmount
     return () => {
-      document.removeEventListener('contextmenu', (e) => {
+      document.removeEventListener("contextmenu", (e) => {
         e.preventDefault();
       });
-      document.removeEventListener('keydown', (e) => {
-        if(e.keyCode === 123) {
+      document.removeEventListener("keydown", (e) => {
+        if (e.keyCode === 123) {
           e.preventDefault();
         }
-        if(e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)) {
+        if (e.ctrlKey && e.shiftKey && e.keyCode === "I".charCodeAt(0)) {
           e.preventDefault();
         }
-        if(e.ctrlKey && e.shiftKey && e.keyCode === 'C'.charCodeAt(0)) {
+        if (e.ctrlKey && e.shiftKey && e.keyCode === "C".charCodeAt(0)) {
           e.preventDefault();
         }
-        if(e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)) {
+        if (e.ctrlKey && e.shiftKey && e.keyCode === "J".charCodeAt(0)) {
           e.preventDefault();
         }
-        if(e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) {
+        if (e.ctrlKey && e.keyCode === "U".charCodeAt(0)) {
           e.preventDefault();
         }
       });
@@ -67,7 +72,7 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<Login />} />
-          
+          <Route element={<PrivateRoutes />}>
             <Route path="/" element={<Rounds />} />
             <Route path="/round1" element={<Round1 />} />
             <Route path="/round2" element={<Round2 />} />
@@ -75,7 +80,7 @@ function App() {
             <Route path="/round4" element={<Round4 />} />
             <Route path="/round5" element={<Round5 />} />
             <Route path="/leaderboard" element={<Leaderboard />} />
-         
+          </Route>
         </Routes>
       </AuthProvider>
     </Router>
